304e89c5b8f83ac8568cf8c137c1292aca9260c7
choshoeah <josh.chung@hotmail.com>
2014-12-14 22:53:56 +1100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -30,10 +30,10 @@
     }
 // put new methods here
 
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *) event
-    {
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get every time the player touches the screen
     [character flap];
 }
 
     @end
+
